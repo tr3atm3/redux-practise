f7806793efdcb57aa980a58783037131ee04e7dc
Susanto <sushantomahato1@gmail.com>
2024-04-10 14:54:25 +0530

    fetching and uploading data from redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,66 @@ import Products from "./components/Shop/Products";
 import Cart from "./components/Cart/Cart";
 
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 function App() {
   const cartInfo = useSelector((store) => store.cart);
+  const [isUpdatingData, setIsUpdatingData] = useState(false);
+  const [isUploadedSuccess, setIsUploadedSuccess] = useState(false);
+  const [isUploadedDenied, setIsUploadedDenied] = useState(false);
+
+  useEffect(() => {
+    setIsUpdatingData(true);
+    fetch(
+      "https://react-deployment-demo-f24d5-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cartInfo) }
+    )
+      .then((res) => {
+        setIsUpdatingData(false);
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((data) => setIsUploadedSuccess(true))
+      .catch((err) => setIsUploadedDenied(true));
+  }, [cartInfo]);
+  const styleClass = {
+    backgroundColor: isUpdatingData
+      ? "blue"
+      : isUploadedSuccess
+      ? "green"
+      : isUploadedDenied
+      ? "red"
+      : "",
+  };
   return (
-    <Layout>
-      {cartInfo.isCartVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      <div className="sent" style={styleClass}>
+        <p>
+          {isUpdatingData
+            ? "Sending..."
+            : isUploadedSuccess
+            ? "Success"
+            : isUploadedDenied
+            ? "Error!"
+            : ""}
+        </p>
+        <p>
+          {isUpdatingData
+            ? "Sending Cart Data"
+            : isUploadedSuccess
+            ? "Cart data sent successfully"
+            : isUploadedDenied
+            ? "Sending Cart data failed"
+            : ""}
+        </p>
+      </div>
+      <Layout>
+        {cartInfo.isCartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,30 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const products = [
+  {
+    title: "Test",
+    price: 6,
+    description: "This is a first product - amazing!",
+  },
+  {
+    title: "test2",
+    price: 10,
+    description: "This is a second product",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((item) => (
+          <ProductItem
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
